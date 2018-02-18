29b5185f0acc6b5305f30d51408061c9f92b7590
lysergide <lure.of.lysergide@gmail.com>
2018-02-18 20:12:52 +0200

    Step 6 completed. The problems with ObjectId was fixed

--- controllers/lesson.js
+++ controllers/lesson.js
@@ -1,5 +1,4 @@
 const Lesson = require('../models/Lesson');
-const ObjectID = require('mongoose').ObjectID;
 
 /**
  * POST /
@@ -41,47 +40,16 @@ exports.lessonsPage = (req, res) => {
  * lesson detailed page.
  */
 exports.lessonDetailedPage = (req, res) => {
-  const urlId = req.params.id;
-  console.log(urlId);
-  console.log(typeof urlId);
-  //const id = { _id: ObjectId(urlId) };
-  Lesson.findById(new ObjectID(req.params.id))
-    .then((lesson) => {
-      res.render('lesson', {
+  Lesson.findOne({ _id: req.params.id }, (err, lesson) => {
+    if (err) {
+      //res.render('error', {});
+      //req.flash('errors', { msg: 'Account with that email address already exists.' });
+    }
+    if (lesson) {
+      return res.render('lesson', {
         title: 'Lesson ',
-        db: lesson
+        lesson: this
       });
-    });
-};
-
-/*exports.postUpdateProfile = (req, res, next) => {
-  req.assert('email', 'Please enter a valid email address.').isEmail();
-  req.sanitize('email').normalizeEmail({ gmail_remove_dots: false });
-
-  const errors = req.validationErrors();
-
-  if (errors) {
-    req.flash('errors', errors);
-    return res.redirect('/account');
-  }
-
-  User.findById(req.user.id, (err, user) => {
-    if (err) { return next(err); }
-    user.email = req.body.email || '';
-    user.profile.name = req.body.name || '';
-    user.profile.gender = req.body.gender || '';
-    user.profile.location = req.body.location || '';
-    user.profile.website = req.body.website || '';
-    user.save((err) => {
-      if (err) {
-        if (err.code === 11000) {
-          req.flash('errors', { msg: 'The email address you have entered is already associated with an account.' });
-          return res.redirect('/account');
-        }
-        return next(err);
-      }
-      req.flash('success', { msg: 'Profile information has been updated.' });
-      res.redirect('/account');
-    });
+    }
   });
-};*/
+};
